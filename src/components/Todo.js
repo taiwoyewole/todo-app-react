45915082a5573ae04331382c32c98d65b13da789
Taiwo Oyewole <56822133+taiwoyewole@users.noreply.github.com>
2022-03-08 10:53:09 +0100

    update icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -24,10 +24,10 @@ const Todo = ({ text, todo, todos, setTodos }) => {
         <div className="todo">
             <li className={`todo-item ${todo.completed ? "completed" : ""} `}>{text}</li>
             <button onClick= {completeHandler} className="complete-btn">
-                <i className="fas fa-check"></i>
+               <i class="fa-solid fa-check"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
-                <i className="fas fa-trash"></i>
+               <i class="fa-solid fa-trash-can"></i>
             </button>
         </div>
     );
